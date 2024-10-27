8dfc00f0e1aa8c0cf3f02ac7ac39df7480274c1d
Chung Phong <phonghoang0205@gmail.com>
2024-10-27 23:45:35 +0700

    Làm trang Danh sách tour theo Danh mục (bên client)

--- controllers/client/tour.controller.ts
+++ controllers/client/tour.controller.ts
@@ -1,18 +1,65 @@
 import { Request, Response } from "express";
 import Tour from "../../models/tour.model";
- 
-// [GET] /tours
+import sequelize from "../../config/database";
+import { QueryTypes } from "sequelize";
+
+// [GET] /tours/:slugCategory
 export const index = async (req: Request, res: Response) => {
   // SELECT * FROM tours WHERE deleted = false AND status = "active";
-  const tours = await Tour.findAll({
-    where: {
-      deleted: false,
-      status: "active"
-    },
-    raw: true
-  });
+  // const tours = await Tour.findAll({
+  //   where: {
+  //     deleted: false,
+  //     status: "active"
+  //   },
+  //   raw: true
+  // });
+  const slugCategory = req.params.slugCategory;
+  /* Lấy ra danh sách tour theo slug của categories (Chưa bị xóa trạng thái active, 
+    trả thêm cột giá đặc biệt
+  )
+    SELECT tours.*, price * (1 - discount/100) AS price_special
+    FROM tours
+    JOIN tours_categories ON tours.id = tours_categories.tour_id
+    JOIN categories ON tours_categories.category_id = categories.id
+    WHERE
+      categories.slug = `${slugCategory}`
+      AND categories.deleted = false
+      AND categories.status = 'active'
+      AND tours.deleted = false
+      AND tours.status = 'active';
+  */
+  const tours = await sequelize.query(
+    `
+      SELECT tours.*, ROUND(price * (1 - discount/100)) AS price_special
+      FROM tours
+      JOIN tours_categories ON tours.id = tours_categories.tour_id
+      JOIN categories ON tours_categories.category_id = categories.id
+      WHERE
+        categories.slug = '${slugCategory}'
+        AND categories.deleted = false
+        AND categories.status = 'active'
+        AND tours.deleted = false
+        AND tours.status = 'active';
+    `,
+    {
+      type: QueryTypes.SELECT,
+    }
+  );
+
+
+  for (const item of tours) {
+    if (item["images"]) {
+      const arrayImage = JSON.parse(item["images"]);
+      if (arrayImage.length > 0) {
+        item["image"] = arrayImage[0];
+      }
+    }
+    item["price_special"] = parseInt(item["price_special"]);
+  }
+
+
   res.render("client/pages/tours/index", {
     pageTitle: "Danh sách tour",
-    tours: tours
+    tours: tours,
   });
 };
