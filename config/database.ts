3be174af58a86baf66fff0a9498c762a6ea8cffe
Chung Phong <phonghoang0205@gmail.com>
2024-10-27 13:08:33 +0700

    Kết nối database

---
+++ config/database.ts
@@ -0,0 +1,24 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DATABASE_NAME, //Tên database
+  process.env.DATABASE_USERNAME, //Tên database, //Username đăng nhập
+  process.env.DATABASE_PASSWORD, //Tên database, // Mật khẩu
+  {
+    host: process.env.DATABASE_HOST,
+    dialect: "mysql",
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log(">>>>Connection successfully.");
+  })
+  .catch((error) => {
+    console.error("Unable to connect to the database: ", error);
+  });
+
+export default sequelize;
--- index.ts
+++ index.ts
@@ -1,13 +1,18 @@
 import express, { Express, Request, Response } from "express";
+import sequelize from "./config/database";
+import dotenv from "dotenv";
+
+dotenv.config();
+sequelize;
 
 const app: Express = express();
-const port: number = 3000;
+const port: number | string = process.env.PORT || 3000;
 
-
-
+app.set("views", "./views");
+app.set("view engine", "pug");
 
 app.get("/tours", (req: Request, res: Response) => {
-  res.send("Danh sách tour");
+  res.render("client/pages/tours/index");
 });
 
 app.listen(port, () => {
