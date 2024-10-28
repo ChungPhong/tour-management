e826add8fc708db5a56022ce254d0d38956f156e
Chung Phong <phonghoang0205@gmail.com>
2024-10-28 11:23:28 +0700

    Tạo khung giao diện

--- routes/client/tour.route.ts
+++ routes/client/tour.route.ts
@@ -5,4 +5,5 @@ import * as controller from "../../controllers/client/tour.controller";
 
 
 router.get("/:slugCategory", controller.index);
+router.get("/detail/:slugTour", controller.detail);
 export const tourRoute = router;
