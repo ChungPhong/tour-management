f45f59b7f2be0e48cb741e48d61af99796b1b167
Chung Phong <phonghoang0205@gmail.com>
2024-10-29 11:02:19 +0700

    Tạo giao diện trang đặt hàng thành công

--- routes/client/order.route.ts
+++ routes/client/order.route.ts
@@ -1,5 +1,8 @@
 import express from "express";
 const router = express.Router();
 import * as controller from "../../controllers/client/order.controller";
+
 router.post("/", controller.index);
+router.get("/success/:orderCode", controller.success);
+
 export const orderRoute = router;
